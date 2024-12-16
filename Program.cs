005d032e58710c8c7fa7f453eea8c03f8e00fe50
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2024-12-16 08:56:45 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿//*****************************************************************************
+//** 3264. Final Array State After K Multiplication Operations I    leetcode **
+//*****************************************************************************
+
+int* getFinalState(int* nums, int numsSize, int k, int multiplier, int* returnSize) 
+{
+    while (k--) 
+    {
+        int maxIndex = 0;
+        for (int i = 1; i < numsSize; i++) 
+        {
+            if (nums[i] < nums[maxIndex] || 
+               (nums[i] == nums[maxIndex] && i < maxIndex)) 
+            {
+                maxIndex = i;
+            }
+        }
+//        printf("nums[%d] = %d * %d\n",maxIndex,nums[maxIndex],k);
+        nums[maxIndex] *= multiplier;
+    }
+
+    *returnSize = numsSize;
+    return nums;
+}
